12c7ace4b521f2592d5bd9c27bc6cdf5edfdef4a
hsun <hsun@owneriq.com>
2016-10-31 09:56:11 -0400

    update HDFS

--- src/main/scala-2.11/com/owneriq/wh/hdfs/SimpleApp.scala
+++ src/main/scala-2.11/com/owneriq/wh/hdfs/SimpleApp.scala
@@ -15,6 +15,8 @@ object SimpleApp {
 
     val logData = sc.textFile(logFile, 2).cache()
 
+    logData.saveAsTextFile("/user/hsun/test_data/campaign_conversion_receipts/")
+
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
